e0987ad239544ba0492cf635e0b5686946bebf60
Yuvaraj kurri <kurriyuvaraj13@gmail.com>
2026-06-18 09:35:46 +0530

    dotenv added

--- server/middlewares/authMiddleWare.js
+++ server/middlewares/authMiddleWare.js
@@ -1,23 +1,25 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+dotenv.config({ path: path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../.env') });
 
-
-dotenv.config();
 
 export const authMiddleware = (req, res, next) => {
-const token = req.headers.authorization?.split(' ')[1] || req.cookies?.token || req.body.token; // Assuming token is sent as "Bearer" or in cookies
+    const token = req.headers.authorization?.split(' ')[1] || req.cookies?.token || req.body.token; // Assuming token is sent as "Bearer" or in cookies
+     if (!token) {
+        return res.status(401).json({ message: "Access denied. No token provided." });
+    }
 
-if (!token) {
-    return res.status(401).json({ message: "Access denied. No token provided." });
-}
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET); 
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Invalid or expired token." });
+    }
 
-try {
-    const decoded = jwt.verify(token, "jwttokensecretkey"); // process.env.JWT_SECRET
-    req.user = decoded;
-    next();
-} catch (error) {
-    return res.status(401).json({ message: "Invalid or expired token." });
-}
 
 };
 
+ 
